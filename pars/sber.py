bb4a5a4f8a7374f9ff2911ab4a185f2e29673a12
PolarJaba <berttalise@gmail.com>
2023-10-10 00:13:07 +0300

    current

--- pars/sber.py
+++ pars/sber.py
@@ -6,75 +6,114 @@ import time
 import datetime
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
 
 import pandas as pd
 
-profs = pd.read_csv('profs.txt', sep=',', header=None, names=['profs_names', 'short_name'])
-df = pd.DataFrame(columns=['link', 'name', 'location',
-                           'company', 'date'])
-
 cur_date = datetime.datetime.now().strftime('%Y-%m-%d')
 cur_year = datetime.datetime.now().year
+profs = pd.read_csv('profs.txt', sep=',', header=None, names=['profs_names'])
+df = pd.DataFrame(columns=['link', 'name', 'location',
+                           'company', 'date', 'date_of_download'])
 
-with webdriver.Chrome() as browser:
 
-    # заход на сайт
-    browser.get(c.sber_base_link)
-    browser.maximize_window()
-    browser.delete_all_cookies()
-    time.sleep(2)
+# ООП версия
 
-    for i in range(0, len(profs)):
-        input_str = browser.find_element(By.XPATH,
-                                         '/html/body/div/div/div[2]/div[3]/div/div/div[2]/div/div/div/div/input')
+class SberJobParser:
 
-        input_str.send_keys(f"{profs['profs_names'][i]}")
-        click_button = browser.find_element(By.XPATH,
-                                            '/html/body/div/div/div[2]/div[3]/div/div/div[2]/div/div/div/div/button')
-        click_button.click()
-        time.sleep(5)
+    def __init__(self, vac_type):
 
-        # Прокрутка вниз до конца страницы
-        page_height = browser.execute_script('return document.body.scrollHeight')
+        # Передаем название вакансии
+        self.vac_type = vac_type
+        self.browser = webdriver.Chrome()
+        self.browser.get(self.url)
+        self.browser.maximize_window()
+        self.browser.delete_all_cookies()
+        time.sleep(2)
 
-        while True:
-            browser.find_element(By.TAG_NAME, 'body').send_keys(Keys.END)
-            time.sleep(2)
+    def scroll_down_page(self, page_height=0):
 
-            new_page_height = browser.execute_script('return document.body.scrollHeight')
+        # Скролл до конца страницы
+        self.browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(2)
 
-            if new_page_height == page_height:
-                break
-            else:
-                page_height = new_page_height
+        new_page_height = self.browser.execute_script('return document.body.scrollHeight')
+        if new_page_height > page_height:
+            self.scroll_down_page(new_page_height)
 
-        # Подсчет количества предложений
-        vacs_bar = browser.find_element(By.XPATH, '/html/body/div/div/div[2]/div[3]/div/div/div[3]/div/div[3]/div[2]')
-        vacs = vacs_bar.find_elements(By.TAG_NAME, 'div')
+    url = c.sber_base_link
 
-        vacs = [div for div in vacs if 'styled__Card-sc-192d1yv-1' in str(div.get_attribute('class'))]
-        print(f"Парсим вакансии по запросу: {profs['profs_names'][i]}")
-        print(f"Количество: " + str(len(vacs)) + "\n")
+    def stop(self):
 
-        for vac in vacs:
-            vac_info = {}
-            vac_info['link'] = vac.find_element(By.TAG_NAME, 'a').get_attribute('href')
-            # vac_info['name'] = vac.find_element(By.CLASS_NAME, 'Text-sc-36c35j-0')
-            data = vac.find_elements(By.CLASS_NAME, 'Text-sc-36c35j-0')
-            vac_info['name'] = data[0].text
-            vac_info['location'] = data[2].text
-            vac_info['company'] = data[3].text
-            vac_info['date'] = data[4].text
-            df.loc[len(df)] = vac_info
+        # Выход из Webdriver selenium
+        self.browser.quit()
 
-        input_str.clear()
+    def find_vacancies(self, profs, df):
 
-        # Раскомментировать для загрузки данных в файлы по запросам
-        # df.to_csv(f"loaded_data/{cur_year}/sber/{profs['short_name'][i]}-{cur_date}.txt", index=False, sep=';')
-        # df = df.drop(df.index[0:])
+        # Поиск и запись вакансий на поисковой странице
+        for prof in profs['profs_names']:
+            input_str = self.browser.find_element(By.XPATH,
+                                                  '/html/body/div/div/div[2]/div[3]/div/div/div[2]/div/div/div/div/input')
 
-df = df.drop_duplicates()
-df.to_csv(f"loaded_data/{cur_year}/sber/all_{cur_date}.txt", index=False, sep=';')
+            input_str.send_keys(f"{prof}")
+            click_button = self.browser.find_element(By.XPATH,
+                                                     '/html/body/div/div/div[2]/div[3]/div/div/div[2]/div/div/div/div/button')
+            click_button.click()
+            time.sleep(5)
+
+            # Прокрутка вниз до конца страницы
+            self.scroll_down_page()
+
+            try:
+                # Подсчет количества предложений
+                vacs_bar = self.browser.find_element(By.XPATH,
+                                                     '/html/body/div/div/div[2]/div[3]/div/div/div[3]/div/div[3]/div[2]')
+                vacs = vacs_bar.find_elements(By.TAG_NAME, 'div')
+
+                vacs = [div for div in vacs if 'styled__Card-sc-192d1yv-1' in str(div.get_attribute('class'))]
+                print(f"Количество вакансий по запросу: '{prof}': " + str(len(vacs)) + "\n")
+
+                for vac in vacs:
+                    vac_info = {}
+                    vac_info['link'] = vac.find_element(By.TAG_NAME, 'a').get_attribute('href')
+                    # vac_info['name'] = vac.find_element(By.CLASS_NAME, 'Text-sc-36c35j-0')
+                    data = vac.find_elements(By.CLASS_NAME, 'Text-sc-36c35j-0')
+                    vac_info['name'] = data[0].text
+                    vac_info['location'] = data[2].text
+                    vac_info['company'] = data[3].text
+                    vac_info['date'] = data[4].text
+                    vac_info['date_of_download'] = datetime.datetime.now().strftime('%Y-%m-%d')
+                    df.loc[len(df)] = vac_info
+                input_str.clear()
+
+            except:
+                input_str.clear()
+                pass
+
+        self.df = df.drop_duplicates()
+
+    def save_df(self):
+        self.df.to_csv(f"all_{cur_date}.txt", index=False, sep=';')
+
+
+    """    def find_descriptions(self, df):
+
+        # Парсинг описаний
+        for descr in range(len(df)):
+            link = df.loc[descr, 'link']
+            self.browser.get(link)
+            self.browser.delete_all_cookies()
+            time.sleep(3)
+
+            df.loc[descr, 'description'] = self.browser.find_element(By.CLASS_NAME, 'Box-sc-159i47a-0').text
+
+        time.sleep(600)"""
+
+
+parser = SberJobParser(profs)
+parser.find_vacancies(profs, df)
+# parser.find_descriptions(df)
+parser.save_df()
+parser.stop()
+
 print("Общее количество вакансий: " + str(len(df)) + "\n")
-
