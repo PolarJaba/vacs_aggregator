62f768f7349193ec50217fdfb5459cf0affb4314
PolarJaba <berttalise@gmail.com>
2023-10-11 15:53:13 +0300

    sber OOP v., fixed dateform., added descr vacs parsing

--- pars/sber.py
+++ pars/sber.py
@@ -1,35 +1,32 @@
-# import requests
-# from bs4 import BeautifulSoup
 import config as c
 
 import time
 import datetime
+import dateparser
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
 import pandas as pd
 
-cur_date = datetime.datetime.now().strftime('%Y-%m-%d')
-cur_year = datetime.datetime.now().year
+
 profs = pd.read_csv('profs.txt', sep=',', header=None, names=['profs_names'])
-df = pd.DataFrame(columns=['link', 'name', 'location',
-                           'company', 'date', 'date_of_download'])
+url_sber = c.sber_base_link
 
-
-# ООП версия
 
 class SberJobParser:
 
-    def __init__(self, vac_type):
+    def __init__(self, url_sber, profs):
 
         # Передаем название вакансии
-        self.vac_type = vac_type
         self.browser = webdriver.Chrome()
-        self.browser.get(self.url)
+        self.url_sber = url_sber
+        self.browser.get(self.url_sber)
         self.browser.maximize_window()
         self.browser.delete_all_cookies()
         time.sleep(2)
+        self.profs = profs
+        self.df = pd.DataFrame(columns=['link', 'name', 'location', 'company', 'date', 'description'])
 
     def scroll_down_page(self, page_height=0):
 
@@ -41,17 +38,15 @@ class SberJobParser:
         if new_page_height > page_height:
             self.scroll_down_page(new_page_height)
 
-    url = c.sber_base_link
-
     def stop(self):
 
         # Выход из Webdriver selenium
         self.browser.quit()
 
-    def find_vacancies(self, profs, df):
+    def find_vacancies(self):
 
         # Поиск и запись вакансий на поисковой странице
-        for prof in profs['profs_names']:
+        for prof in self.profs['profs_names']:
             input_str = self.browser.find_element(By.XPATH,
                                                   '/html/body/div/div/div[2]/div[3]/div/div/div[2]/div/div/div/div/input')
 
@@ -76,44 +71,48 @@ class SberJobParser:
                 for vac in vacs:
                     vac_info = {}
                     vac_info['link'] = vac.find_element(By.TAG_NAME, 'a').get_attribute('href')
-                    # vac_info['name'] = vac.find_element(By.CLASS_NAME, 'Text-sc-36c35j-0')
                     data = vac.find_elements(By.CLASS_NAME, 'Text-sc-36c35j-0')
                     vac_info['name'] = data[0].text
                     vac_info['location'] = data[2].text
                     vac_info['company'] = data[3].text
                     vac_info['date'] = data[4].text
                     vac_info['date_of_download'] = datetime.datetime.now().strftime('%Y-%m-%d')
-                    df.loc[len(df)] = vac_info
+                    self.df.loc[len(self.df)] = vac_info
                 input_str.clear()
 
             except:
                 input_str.clear()
                 pass
 
-        self.df = df.drop_duplicates()
+        self.df = self.df.drop_duplicates(subset=['link'], keep='last')
+        self.df['date'] = self.df['date'].apply(lambda x: dateparser.parse(x).strftime('%Y-%m-%d'))
+        print('Всего:', len(self.df.drop_duplicates(subset=['link'], keep='last')))
+        self.df.to_csv(f"all_shorts.txt", index=False, sep=';')
 
-    def save_df(self):
-        self.df.to_csv(f"all_{cur_date}.txt", index=False, sep=';')
-
-
-    """    def find_descriptions(self, df):
+    def find_descriptions(self):
 
         # Парсинг описаний
-        for descr in range(len(df)):
-            link = df.loc[descr, 'link']
+        for descr in self.df.index:
+            link = self.df.loc[descr, 'link']
             self.browser.get(link)
             self.browser.delete_all_cookies()
             time.sleep(3)
+            #texts_elems = self.browser.find_elements(By.CLASS_NAME, 'Box-sc-159i47a-0')
+            try:
+                self.df.loc[descr, 'description'] = str(self.browser.find_element(By.XPATH, '/html/body/div[1]/div/div[2]/div[3]/div/div/div[3]/div[3]').text.replace(';', ''))
+            except:
+                self.df.loc[descr, 'description'] = None
 
-            df.loc[descr, 'description'] = self.browser.find_element(By.CLASS_NAME, 'Box-sc-159i47a-0').text
 
-        time.sleep(600)"""
+    def save_df(self):
+
+        self.df.to_csv(f"all_3.txt", index=False, sep='|')
+        # Вывод результатов парсинга
+        print("Общее количество вакансий: " + str(len(self.df)) + "\n")
 
 
-parser = SberJobParser(profs)
-parser.find_vacancies(profs, df)
-# parser.find_descriptions(df)
+parser = SberJobParser(url_sber, profs)
+parser.find_vacancies()
+parser.find_descriptions()
 parser.save_df()
 parser.stop()
-
-print("Общее количество вакансий: " + str(len(df)) + "\n")
